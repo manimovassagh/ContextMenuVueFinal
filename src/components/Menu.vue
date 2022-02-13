afaa703096fe1a4dd535812f80bfd1386c24c610
Mani Movassagh <74688476+manimovassagh@users.noreply.github.com>
2022-02-13 01:22:42 +0100

    Initial commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,49 @@
+<template>
+    <div @contextmenu="show" class="ma-0 pa-0">
+        <slot></slot>
+
+        <v-menu
+            v-model="showMenu"
+            :position-x="x"
+            :position-y="y"
+            absolute
+            offset-y
+            class="ma-0 pa-0"
+        >
+            <v-list>
+                <v-list-item v-for="(item, index) in items" :key="index" link>
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </div>
+</template>
+<script>
+export default {
+    data: () => ({
+        showMenu: false,
+        x: 0,
+        y: 0,
+        items: [
+            { title: 'Click Me' },
+            { title: 'Click Me' },
+            { title: 'Click Me' },
+            { title: 'Click Me 2' }
+        ]
+    }),
+
+    methods: {
+        show(e) {
+            e.preventDefault()
+            this.showMenu = false
+            this.x = e.clientX
+            this.y = e.clientY
+            this.$nextTick(() => {
+                this.showMenu = true
+            })
+        }
+    }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/layout.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app>
+        <v-main>
+            <Menu>
+                <v-container class="ma-0 pa-0" fluid>
+                    <div>check</div>
+                    <div>check</div>
+                    <div>check</div>
+                    <div>check</div>
+                    <div>check</div>
+                </v-container>
+            </Menu>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import Menu from '@/components/Menu.vue'
+export default {
+    components: {
+        Menu
+    },
+    data: () => ({
+        collapseOnScroll: true
+    })
+}
+</script>
+
+<style>
+</style>
